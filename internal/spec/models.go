3808684ed0098ce58f4d653d13e46d20c71f8ea2
sunboyy <sura_sun@hotmail.com>
2021-01-16 13:36:44 +0700

    Add functionality to parse repository specification from method name (#2)

---
+++ internal/spec/models.go
@@ -0,0 +1,46 @@
+package spec
+
+import "github.com/sunboyy/repogen/internal/code"
+
+// QueryMode one or many
+type QueryMode string
+
+// query mode constants
+const (
+	QueryModeOne  QueryMode = "ONE"
+	QueryModeMany QueryMode = "MANY"
+)
+
+// RepositorySpec is a specification generated from the repository interface
+type RepositorySpec struct {
+	InterfaceName string
+	Methods       []MethodSpec
+}
+
+// MethodSpec is a method specification inside repository specification
+type MethodSpec struct {
+	Name      string
+	Params    []code.Param
+	Returns   []code.Type
+	Operation Operation
+}
+
+// Operation is an interface for any kind of operation
+type Operation interface {
+}
+
+// FindOperation is a method specification for find operations
+type FindOperation struct {
+	Mode  QueryMode
+	Query Query
+}
+
+// Query is a condition of querying the database
+type Query struct {
+	Fields []string
+}
+
+// NumberOfArguments returns number of arguments required to perform the query
+func (q Query) NumberOfArguments() int {
+	return len(q.Fields)
+}
---
+++ internal/spec/parser_test.go
@@ -0,0 +1,259 @@
+package spec_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sunboyy/repogen/internal/code"
+	"github.com/sunboyy/repogen/internal/spec"
+)
+
+type TestCase struct {
+	Name           string
+	Interface      code.Interface
+	ExpectedOutput spec.RepositorySpec
+}
+
+func TestParseRepositoryInterface(t *testing.T) {
+	testTable := []TestCase{
+		{
+			Name: "interface spec",
+			Interface: code.Interface{
+				Name: "UserRepository",
+			},
+			ExpectedOutput: spec.RepositorySpec{
+				InterfaceName: "UserRepository",
+			},
+		},
+		{
+			Name: "FindOneByArg method",
+			Interface: code.Interface{
+				Name: "UserRepository",
+				Methods: []code.Method{
+					{
+						Name: "FindOneByID",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.ExternalType{PackageAlias: "primitive", Name: "ObjectID"}},
+						},
+						Returns: []code.Type{
+							code.PointerType{ContainedType: code.SimpleType("UserModel")},
+							code.SimpleType("error"),
+						},
+					},
+				},
+			},
+			ExpectedOutput: spec.RepositorySpec{
+				InterfaceName: "UserRepository",
+				Methods: []spec.MethodSpec{
+					{
+						Name: "FindOneByID",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.ExternalType{PackageAlias: "primitive", Name: "ObjectID"}},
+						},
+						Returns: []code.Type{
+							code.PointerType{ContainedType: code.SimpleType("UserModel")},
+							code.SimpleType("error"),
+						},
+						Operation: spec.FindOperation{
+							Mode:  spec.QueryModeOne,
+							Query: spec.Query{Fields: []string{"ID"}},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "FindOneByMultiWordArg method",
+			Interface: code.Interface{
+				Name: "UserRepository",
+				Methods: []code.Method{
+					{
+						Name: "FindOneByPhoneNumber",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.SimpleType("string")},
+						},
+						Returns: []code.Type{
+							code.PointerType{ContainedType: code.SimpleType("UserModel")},
+							code.SimpleType("error"),
+						},
+					},
+				},
+			},
+			ExpectedOutput: spec.RepositorySpec{
+				InterfaceName: "UserRepository",
+				Methods: []spec.MethodSpec{
+					{
+						Name: "FindOneByPhoneNumber",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.SimpleType("string")},
+						},
+						Returns: []code.Type{
+							code.PointerType{ContainedType: code.SimpleType("UserModel")},
+							code.SimpleType("error"),
+						},
+						Operation: spec.FindOperation{
+
+							Mode:  spec.QueryModeOne,
+							Query: spec.Query{Fields: []string{"PhoneNumber"}},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "FindByArg method",
+			Interface: code.Interface{
+				Name: "UserRepository",
+				Methods: []code.Method{
+					{
+						Name: "FindByCity",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.SimpleType("string")},
+						},
+						Returns: []code.Type{
+							code.ArrayType{ContainedType: code.PointerType{ContainedType: code.SimpleType("UserModel")}},
+							code.SimpleType("error"),
+						},
+					},
+				},
+			},
+			ExpectedOutput: spec.RepositorySpec{
+				InterfaceName: "UserRepository",
+				Methods: []spec.MethodSpec{
+					{
+						Name: "FindByCity",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.SimpleType("string")},
+						},
+						Returns: []code.Type{
+							code.ArrayType{ContainedType: code.PointerType{ContainedType: code.SimpleType("UserModel")}},
+							code.SimpleType("error"),
+						},
+						Operation: spec.FindOperation{
+							Mode:  spec.QueryModeMany,
+							Query: spec.Query{Fields: []string{"City"}},
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "FindAll method",
+			Interface: code.Interface{
+				Name: "UserRepository",
+				Methods: []code.Method{
+					{
+						Name: "FindAll",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+						},
+						Returns: []code.Type{
+							code.ArrayType{ContainedType: code.PointerType{ContainedType: code.SimpleType("UserModel")}},
+							code.SimpleType("error"),
+						},
+					},
+				},
+			},
+			ExpectedOutput: spec.RepositorySpec{
+				InterfaceName: "UserRepository",
+				Methods: []spec.MethodSpec{
+					{
+						Name: "FindAll",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+						},
+						Returns: []code.Type{
+							code.ArrayType{ContainedType: code.PointerType{ContainedType: code.SimpleType("UserModel")}},
+							code.SimpleType("error"),
+						},
+						Operation: spec.FindOperation{
+							Mode: spec.QueryModeMany,
+						},
+					},
+				},
+			},
+		},
+		{
+			Name: "FindByArgAndArg method",
+			Interface: code.Interface{
+				Name: "UserRepository",
+				Methods: []code.Method{
+					{
+						Name: "FindByCityAndGender",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.SimpleType("string")},
+							{Type: code.SimpleType("Gender")},
+						},
+						Returns: []code.Type{
+							code.ArrayType{ContainedType: code.PointerType{ContainedType: code.SimpleType("UserModel")}},
+							code.SimpleType("error"),
+						},
+					},
+				},
+			},
+			ExpectedOutput: spec.RepositorySpec{
+				InterfaceName: "UserRepository",
+				Methods: []spec.MethodSpec{
+					{
+						Name: "FindByCityAndGender",
+						Params: []code.Param{
+							{Type: code.ExternalType{PackageAlias: "context", Name: "Context"}},
+							{Type: code.SimpleType("string")},
+							{Type: code.SimpleType("Gender")},
+						},
+						Returns: []code.Type{
+							code.ArrayType{ContainedType: code.PointerType{ContainedType: code.SimpleType("UserModel")}},
+							code.SimpleType("error"),
+						},
+						Operation: spec.FindOperation{
+							Mode:  spec.QueryModeMany,
+							Query: spec.Query{Fields: []string{"City", "Gender"}},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	structModel := code.Struct{
+		Name: "UserModel",
+		Fields: code.StructFields{
+			{
+				Name: "ID",
+				Type: code.ExternalType{PackageAlias: "primitive", Name: "ObjectID"},
+			},
+			{
+				Name: "PhoneNumber",
+				Type: code.SimpleType("string"),
+			},
+			{
+				Name: "Gender",
+				Type: code.SimpleType("Gender"),
+			},
+			{
+				Name: "City",
+				Type: code.SimpleType("string"),
+			},
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.Name, func(t *testing.T) {
+			actualSpec, err := spec.ParseRepositoryInterface(structModel, testCase.Interface)
+
+			if err != nil {
+				t.Errorf("Error = %s", err)
+			}
+			if !reflect.DeepEqual(actualSpec, testCase.ExpectedOutput) {
+				t.Errorf("Expected = %v\nReceived = %v", testCase.ExpectedOutput, actualSpec)
+			}
+		})
+	}
+}
